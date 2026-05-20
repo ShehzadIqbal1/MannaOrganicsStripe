788975c2adc2eab1d7480f88e9093b41edea236f
ShehzadIqbal1 <shehzad.iqbal622@gmail.com>
2026-05-21 03:25:24 +0500

    updated nodemailer code

--- config/mailer.js
+++ config/mailer.js
@@ -3,11 +3,14 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
   port: Number(process.env.MAIL_PORT),
-  secure: false,
+  secure: process.env.MAIL_SECURE === "true",
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS
-  }
+  },
+  connectionTimeout: 15000,
+  greetingTimeout: 15000,
+  socketTimeout: 15000
 });
 
-module.exports = transporter;   
+module.exports = transporter;
